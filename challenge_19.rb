cbca88ad2b856973425c8f570c71d12f296bc6e1
A.L <adlawren010@gmail.com>
2021-04-25 20:23:57 -0600

    Add solutions 19 & 20

---
+++ challenge_19.rb
@@ -0,0 +1,23 @@
+require 'pry-byebug'
+
+crypt = CryptCTR.new(nonce: Array.new(8, 0).pack('C*'))
+
+ciphertexts = File.readlines('19.txt').map do |unknown|
+  crypt.encrypt(decode_base64(unknown.strip))
+end
+
+smallest_ciphertext_size = ciphertexts.map(&:size).min
+ciphertext_columns = smallest_ciphertext_size.times.map { '' }
+ciphertexts.map do |ciphertext|
+  ciphertext.chars.take(smallest_ciphertext_size).each.with_index do |char, idx|
+    ciphertext_columns[idx] += char
+  end
+end
+
+cracked_keystream = ciphertext_columns.map do |ciphertext_column|
+  crack_single_char_xor_key(ciphertext_column).chars.first
+end.join('')
+
+ciphertexts.each do |ciphertext|
+  p xor(ciphertext.slice(0, cracked_keystream.size), cracked_keystream)
+end
--- run_all.rb
+++ run_all.rb
@@ -1,2 +1,2 @@
 # Omit challenges which take a long time
-(1..18).each { |v| load "challenge_#{v}.rb" unless [4,6,17].include?(v) }
+(1..20).each { |v| load "challenge_#{v}.rb" unless [4,6,17].include?(v) }
